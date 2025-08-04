2eb4e1ca01d6a7f32ef9624d688d43b52ca7d73c
Abdurahmon <abdurahmonbekmirzayev3@gmail.com>
2025-08-04 13:47:50 +0500

    contact page finished

--- src/pages/Contact/Contact.style.js
+++ src/pages/Contact/Contact.style.js
@@ -2,8 +2,41 @@ import styled from "styled-components";
 
 export const ContactWrapper = styled.div`
   background-color: #1e201e;
+  width: 100%; /* To‘liq kenglikni egallashi uchun */
 `;
 
 export const ContactContainer = styled.div`
+  display: flex;
   margin: 10px auto;
+  width: 1200px; /* Ortiqcha chiqib ketmaslik uchun maksimal kenglik */
+  width: 100%; /* Mavjud kenglikni to‘liq ishlatish */
+  padding: 0 20px;
+`;
+
+export const ContactImageWrapper = styled.div`
+  width: 446px;
+  height: 310px;
+  background-color: #d9d9d9;
+`;
+
+export const LeftSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 70%;
+`;
+
+export const RightSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-end;
+  width: 50%;
+`;
+
+export const TextSection = styled.div`
+  margin-top: 100px;
+  margin-bottom: 50px;
+  margin-left: 66px;
 `;
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,19 +1,38 @@
 import React from "react";
-import { ContactContainer, ContactWrapper } from "./Contact.style";
+import {
+  ContactContainer,
+  ContactImageWrapper,
+  ContactWrapper,
+  LeftSide,
+  RightSide,
+  TextSection,
+} from "./Contact.style";
 import { Input, Typography } from "../../components";
 
 const ContactPage = () => {
   return (
     <ContactWrapper>
       <ContactContainer className="container">
-        <Typography color="#d9d9d9">LET'S CONNECT</Typography>
-        <Input text="Your Name" placeholder="John Doe" />
-        <Input text="Your Email" placeholder="john@gmail.com" />
-        <Input
-          type="textarea"
-          text="Your Message"
-          placeholder="Type your message here..."
-        />
+        <LeftSide>
+          <Typography color="#d9d9d9">LET'S CONNECT</Typography>
+          <Input text="Your Name" placeholder="John Doe" />
+          <Input text="Your Email" placeholder="john@gmail.com" />
+          <Input
+            type="textarea"
+            text="Your Message"
+            placeholder="Type your message here..."
+          />
+        </LeftSide>
+        <RightSide>
+          <TextSection>
+            <Typography color="#d9d9d9" type="paragraphText">
+              I'm always open to new opportunities, creative ideas, or just a
+              good conversation. Feel free to drop me a message and I’ll get
+              back to you as soon as I can.
+            </Typography>
+          </TextSection>
+          <ContactImageWrapper />
+        </RightSide>
       </ContactContainer>
     </ContactWrapper>
   );
